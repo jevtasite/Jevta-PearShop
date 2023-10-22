8a80c56c7d71c4f58a3356f290d3427338ba9772
jevtasite <jevta.site@gmail.com>
2023-10-22 21:27:55 +0200

    fixed greb greb and optimized it for touchscreen

--- js/main.js
+++ js/main.js
@@ -71,63 +71,57 @@ startCounting("shops", 15, 250);
 
 //GREB GREB 
 var bridge = document.getElementById("bridge"),
-bridgeCanvas = bridge.getContext('2d'),
-brushRadius = (bridge.width / 100) * 5,
-img = new Image();
+    bridgeCanvas = bridge.getContext('2d'),
+    brushRadius = (bridge.width / 100) * 5,
+    img = new Image();
 
-if (brushRadius < 50) { brushRadius = 50 }
-
-img.onload = function(){  
-	bridgeCanvas.drawImage(img, 0, 0, bridge.width, bridge.height);
+if (brushRadius < 50) {
+    brushRadius = 50;
 }
+
+img.onload = function () {
+    bridgeCanvas.drawImage(img, 0, 0, bridge.width, bridge.height);
+};
 img.loc = './images/';
 img.filename = 'vegetable3.jpg';
 if (window.devicePixelRatio >= 2) {
-	var nameParts = img.filename.split('.');
-	img.src = img.loc + nameParts[0]+"-2x"+"."+nameParts[1];
+    var nameParts = img.filename.split('.');
+    img.src = img.loc + nameParts[0] + "-2x" + "." + nameParts[1];
 } else {
-	img.src = img.loc + img.filename;
-}
-
-function detectLeftButton(event) {
-    if ('buttons' in event) {
-        return event.buttons === 1;
-    } else if ('which' in event) {
-        return event.which === 1;
-    } else {
-        return event.button === 1;
-    }
+    img.src = img.loc + img.filename;
 }
 
 function getBrushPos(xRef, yRef) {
-	var bridgeRect = bridge.getBoundingClientRect();
+    var bridgeRect = bridge.getBoundingClientRect();
     return {
-	  x: Math.floor((xRef-bridgeRect.left)/(bridgeRect.right-bridgeRect.left)*bridge.width),
-	  y: Math.floor((yRef-bridgeRect.top)/(bridgeRect.bottom-bridgeRect.top)*bridge.height)
+        x: Math.floor((xRef - bridgeRect.left) / (bridgeRect.right - bridgeRect.left) * bridge.width),
+        y: Math.floor((yRef - bridgeRect.top) / (bridgeRect.bottom - bridgeRect.top) * bridge.height)
     };
 }
-      
-function drawDot(mouseX,mouseY){
-	bridgeCanvas.beginPath();
-    bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2*Math.PI, true);
+
+function drawDot(mouseX, mouseY) {
+    bridgeCanvas.beginPath();
+    bridgeCanvas.arc(mouseX, mouseY, brushRadius, 0, 2 * Math.PI, true);
     bridgeCanvas.fillStyle = '#000';
     bridgeCanvas.globalCompositeOperation = "destination-out";
     bridgeCanvas.fill();
 }
 
-bridge.addEventListener("mousemove", function(e) {
-	var brushPos = getBrushPos(e.clientX, e.clientY);
-  var leftBut = detectLeftButton(e);
-  if (leftBut == 1) {
-		drawDot(brushPos.x, brushPos.y);
-  }
+function handleDrawEvent(x, y) {
+    var brushPos = getBrushPos(x, y);
+    drawDot(brushPos.x, brushPos.y);
+}
+
+bridge.addEventListener("mousemove", function (e) {
+    if (e.buttons === 1) {
+        handleDrawEvent(e.clientX, e.clientY);
+    }
 }, false);
 
-bridge.addEventListener("touchmove", function(e) {
+bridge.addEventListener("touchmove", function (e) {
     e.preventDefault();
-    var touch = e.targetTouches[0];
+    var touch = e.touches[0];
     if (touch) {
-    var brushPos = getBrushPos(touch.pageX, touch.pageY);
-        drawDot(brushPos.x, brushPos.y);
+        handleDrawEvent(touch.clientX, touch.clientY);
     }
 }, false);
